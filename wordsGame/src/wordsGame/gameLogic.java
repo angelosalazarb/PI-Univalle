617e41fa5c9d91293b05d6b95ea8d0314d7cd263
angelosalazarb <angelosalazarb@gmail.com>
2021-09-01 19:39:31 -0500

    gameLogic class creation

---
+++ wordsGame/src/wordsGame/gameLogic.java
@@ -0,0 +1,20 @@
+/*
+ * Autors: Juan José Bailon, Ángelo Salazar Bermúdez, Juan José Revelo
+ * Email: angelo.salazar@correounivalle.edu.co
+ */
+
+package wordsGame;
+
+import java.util.ArrayList;
+
+public class gameLogic {
+
+	InputOutput getGameWords;
+	public int lvl, correctWordsCounter, badWordsCounter;
+	private ArrayList<String> importWords;
+	public ArrayList<String> correctWords, droppedWords, userInputWords;
+	
+	
+	
+	
+}
